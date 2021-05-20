88ea5a21786f567d0c788421544678ab76960038
benno90 <benno.90@hotmail.com>
2021-05-20 08:31:36 +0200

    new dynamic adc computation

--- svpwm_current_sampling.py
+++ svpwm_current_sampling.py
@@ -8,6 +8,7 @@ from matplotlib.patches import Rectangle
 from svpwm_prototype import svpwm, _T
 from definitions import *
 
+DEAD_TIME = 32
 
 fig, ax = plt.subplots()
 plt.sca(ax)
@@ -28,24 +29,55 @@ angleSlider = Slider(angleAx, 'angle', 0, 360, valinit=0)
 #sO = Slider(axO, 'O', 0, 1, valinit = 1)
 
 def plotSwitchTime(ax, T):
-
+    global DEAD_TIME
     plt.sca(ax)
     ax.clear()
 
     #t = _T - T
 
     val = np.zeros(4096)
+    valN = np.zeros(4096)
     for i in range(0, 2048):
         if i < T:
             val[i] = 1
+            valN[i] = 0
         else:
             val[i] = 0
+            valN[i] = 1
 
     for i in range(0, 2048):
         val[i + 2048] = val[2047 - i]
+        valN[i + 2048] = valN[2047 - i]
+    
+    val_highside = np.copy(val)
+    val_lowside = np.copy(valN)
+
+    for i in range(1, 4096):
+        # detect rising edge and add dead time on high side signal
+        if val[i - 1] == 0 and val[i] == 1:
+            for k in range(0, DEAD_TIME):
+                l = i + k
+                if l < 4096:
+                    val_highside[l] = 0
+            break
+
+    for i in range(0, 4096):
+        # detect falling edge and add dead time on low side signal
+        if val[i - 1] == 1 and val[i] == 0:
+            for k in range(0, DEAD_TIME):
+                l = i + k
+                if l < 4096:
+                    val_lowside[l] = 0
+            break
+
 
     plt.axis('off')
-    plt.plot(val, color='blue')
+    plt.plot(val_highside, color='red', linewidth=3, label='highside')
+    plt.plot(val_lowside, color='blue', linewidth=3, label='lowside')
+    plt.plot(val, color='yellow', linewidth=1, label='reference')
+
+    if ax == ph3Ax:
+        plt.legend()
 
 
 def plotADC_sample(ax, CCR):
@@ -53,23 +85,46 @@ def plotADC_sample(ax, CCR):
     ax.add_patch(Rectangle((CCR, 0), ADC_SAMPLE_TIME, 1.3))
 
 
-# dyn_adc_state 1 -> phase 1 & 2
-# dyn_adc_state 2 -> phase 2 & 3
-# dyn adc_state_3 -> phaee 3 & 1
+# dyn_adc_state 1 -> measure phase 1 & 2
+# dyn_adc_state 2 -> measure phase 2 & 3
+# dyn adc_state_3 -> measure phase 3 & 1
 def my_dyn_adc_state(switchtime):
-    TRIGGER_DEFAULT = 2020
-    if switchtime[0] > switchtime[2] and switchtime[1] > switchtime[2]:
+    TRIGGER_DEFAULT = 2048
+    TRIGGER_OFFSET_ADC = 100
+
+    tph1N = _T - switchtime[0]
+    tph2N = _T - switchtime[1]
+    tph3N = _T - switchtime[2]
+    
+    if switchtime[2] > switchtime[0] and switchtime[2] > switchtime[1]:
         char_dyn_adc_state = 1
-        CCR4 = TRIGGER_DEFAULT
-    elif switchtime[1] > switchtime[0] and switchtime[2] > switchtime[0]:
+        if switchtime[2] > 1500:
+            if tph1N > 2 * tph3N and tph2N > 2 * tph3N:
+                CCR4 = switchtime[2] - TRIGGER_OFFSET_ADC
+            else:
+                CCR4 = TRIGGER_DEFAULT + tph3N - TRIGGER_OFFSET_ADC
+        else:
+            CCR4 = TRIGGER_DEFAULT
+    elif switchtime[0] > switchtime[1] and switchtime[0] > switchtime[1]:
         char_dyn_adc_state = 2
-        CCR4 = TRIGGER_DEFAULT
+        if switchtime[0] > 1500:
+            if tph2N > 2 * tph1N and tph3N > 2 * tph1N:
+                CCR4 = switchtime[0] - TRIGGER_OFFSET_ADC
+            else:
+                CCR4 = TRIGGER_DEFAULT + tph1N - TRIGGER_OFFSET_ADC
+                #CCR4 = TRIGGER_DEFAULT
+        else:
+            CCR4 = TRIGGER_DEFAULT
     else:
         char_dyn_adc_state = 3
-        CCR4 = TRIGGER_DEFAULT
+        if switchtime[1] > 1500:
+            if tph1N > 2 * tph2N and tph3N > 2 * tph2N:
+                CCR4 = switchtime[1] - TRIGGER_OFFSET_ADC
+            else:
+                CCR4 = TRIGGER_DEFAULT + tph2N - TRIGGER_OFFSET_ADC
+        else:
+            CCR4 = TRIGGER_DEFAULT
     
-    print(char_dyn_adc_state, switchtime[0], switchtime[1], switchtime[2])
-
     return char_dyn_adc_state, CCR4
 
 
@@ -130,9 +185,10 @@ def updatePlot(x):
     ph2Ax.set_title('ph2')
     ph3Ax.set_title('ph3')
 
+
     switchtime = np.array((Tph1, Tph2, Tph3))
-    char_dyn_adc_state, CCR4 = dyn_adc_state(switchtime)
-    #char_dyn_adc_state, CCR4 = my_dyn_adc_state(switchtime)
+    #char_dyn_adc_state, CCR4 = dyn_adc_state(switchtime)
+    char_dyn_adc_state, CCR4 = my_dyn_adc_state(switchtime)
 
     if char_dyn_adc_state == 1:
         plotADC_sample(ph1Ax, CCR4)
@@ -154,5 +210,5 @@ amplitudeSlider.on_changed(updatePlot)
 angleSlider.on_changed(updatePlot)
 # sO.on_changed(update)
 
-
+updatePlot(None)
 plt.show()
